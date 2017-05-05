b3a7671af27181c4877062ca0047ff0786619d78
Philip Hutchins <flipture@gmail.com>
2017-05-05 11:52:10 -0400

    Fixing container name in deploy script

--- dockerfiles/deploy.staging.sh
+++ dockerfiles/deploy.staging.sh
@@ -3,7 +3,7 @@
 PROJECT_NAME=$1
 CONTAINER_NAME=$2
 TAG=$3
-IMAGE=storjlabs/${PROJECT_NAME}:${TAG}
+IMAGE=storjlabs/${CONTAINER_NAME}:${TAG}
 
 kubectl config set-cluster storj-nonprod
 kubectl --namespace storj-staging patch deployment ${PROJECT_NAME} -p"{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${CONTAINER_NAME}\",\"image\":\"${IMAGE}\"}]}}}}"
